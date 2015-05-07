d848a22a02b03ccd31186b0d10b232ea71caf7b5
Rohit Deshpande <rohitdeshpande@gmail.com>
2015-05-07 08:13:24 +0530

    Added problem 4

---
+++ Zelle_solutions/problem1_1.py
@@ -0,0 +1,6 @@
+"""A user-friendly program should print an introduction that tells the user
+what the program does. Modify the program (Section 2.2) to print an
+introduction."""
+
+def main():
+    celsius = input("What is the Celsius temperature")

07238dc52430704d6450bc8138d5230c4a328aa9
Rohit Deshpande <rohitdeshpande@gmail.com>
2015-05-07 10:58:35 +0530

    Added problem 10

--- Zelle_solutions/problem4_10.py
+++ Zelle_solutions/problem4_10.py
@@ -2,10 +2,11 @@ def main():
     string = input("Enter a string: ")
     split_str = string.split()
     count = 0
-    acry = ""
+    sum = 0
     while count < len(split_str):
-        acr = split_str[count][0]
-        acry += acr
+        sum = sum + len(split_str[count])
         count += 1
-    print acry.upper()
+    
+
+
 main()

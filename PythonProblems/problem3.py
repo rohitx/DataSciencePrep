55240b57abb6afbe7a1a4828ebed1fa0c01fb5d2
Rohit Deshpande <rohitdeshpande@gmail.com>
2015-05-07 08:43:21 +0530

    Updated some files

--- PythonProblems/problem3.py
+++ PythonProblems/problem3.py
@@ -18,3 +18,33 @@ checkio('QWERTYqwerty') == False
 checkio('123456123456') == False
 checkio('QwErTy911poqqqq') == True
 """
+def checkio(data):
+    # check the length
+    check_len = len(data) >= 10
+    # check if it has words
+    wo = [w.isalpha() for w in data]
+    check_wo = True in wo
+    # check if it has numbers
+    no = [n.isdigit() for n in data]
+    check_num = True in no
+    # check uppercase
+    up = [u.isupper() for u in data]
+    check_upp = True in up
+    # check lowercase
+    lo = [l.islower() for l in data]
+    check_low = True in lo
+    print check_len, check_wo, check_num, check_upp, check_upp
+    answer = check_len + check_wo + check_num + check_upp + check_low
+    print answer
+    if answer == 5:
+        return True
+    else:
+        return False
+
+print checkio('DHJK87DSKJHWW68D')
+
+
+# Alternate solution
+import re
+return True if re.search("^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*$", data) and len(data) >= 10 else False
+
--- Zelle_solutions/problem1_1.py
+++
@@ -1,6 +0,0 @@
-"""A user-friendly program should print an introduction that tells the user
-what the program does. Modify the program (Section 2.2) to print an
-introduction."""
-
-def main():
-    celsius = input("What is the Celsius temperature")

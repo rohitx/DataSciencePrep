bed00c724d9bddb518f55dbb03afa0ee2ee3d775
Rohit Deshpande <rohitdeshpande@gmail.com>
2015-05-12 14:17:37 +0530

    Added figures for chapter 1

---
+++ NLTK/break_string.py
@@ -0,0 +1,8 @@
+# This program breaks a string into a list of strings
+
+myString = ["This is something I have been working on"]
+
+# add each element of the string to a dummy list
+# if you hit a space:
+    # dump the appended string to another list
+# reset the dummy list
